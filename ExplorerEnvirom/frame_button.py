8f02932c172faad060088cc430e97751eb00ca6c
Dmitry Levin <d.levin@a1qa.com>
2026-01-29 13:41:34 +0300

    change

--- ExplorerEnvirom/frame_button.py
+++ ExplorerEnvirom/frame_button.py
@@ -4,11 +4,11 @@ class DifferenceFrame:
 
     def frame_for_button(self):
         frame_for_button = Frame(self, bg='yellow')
-        frame_for_button.place(relx=0, rely=0, relwidth=1, relheight=0.08)
+        frame_for_button.place(relx=0, rely=0, relwidth=1, relheight=0.09)
         return
 
     def frame_for_library(self):
         frame_for_library = Frame(self)
-        frame_for_library.place(relx=0, rely=0.08, relwidth=1, relheight=1)
+        frame_for_library.place(relx=0, rely=0.09, relwidth=1, relheight=1)
 
 
